240fac31c24694d8e6fe11c19f49319ff4c923fd
Piyush Sharma <54810697+spiyush0212@users.noreply.github.com>
2020-07-06 15:35:16 +0530

    Add files via upload

---
+++ Projects/1-calculator.rb
@@ -0,0 +1,18 @@
+puts "WELCOME TO THE BASIC CALCULATOR"
+puts()
+
+puts "Enter 1st number: "
+num1 = gets.chomp().to_f
+puts "Enter 2nd number: "
+num2 = gets.chomp().to_f
+
+# sum = num1 + num2
+# sub = num1 - num2
+# mul = num1 * num2
+# div = num1 / num2
+
+puts "THE RESULTS ARE: "
+puts (num1 + num2)
+puts (num1 - num2)
+puts (num1 * num2)
+puts (num1 / num2)
---
+++ Projects/2-mad_lib_game.rb
@@ -0,0 +1,15 @@
+puts "WELCOME TO THE MAD LIBS GAME"
+puts()
+
+puts "Enter a color"
+color = gets.chomp()
+puts "Enter a plural noun"
+noun = gets.chomp()
+puts "Enter a person's name"
+name = gets.chomp()
+
+puts "THE MAD LIBS "
+puts()
+puts ("Roses are " + color)
+puts (noun + " are blue")
+puts ("I Love " + name)
